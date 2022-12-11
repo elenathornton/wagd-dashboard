acb361a86fb90d9bd8162ba90efaf107a01da49a
“lthornton3” <elena.thornton@gmail.com>
2022-12-11 12:20:00 -0500

    default request

--- api/app.py
+++ api/app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, request, session
+import os
 
 api = Flask(__name__)
 
 @api.route('/')
 def start():
     response_body = {
-        "name": "Test",
-        "about" :"This is a test"
+        "Status": "Server is live."
     }
 
     return response_body
+
+
+@api.route('/upload', methods=['POST'])
+def fileUpload():
+    target=os.path.join('/','test_docs')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    print("upload`")
+    file = request.files['file']
+    filename = file.filename
+    print(filename)
+    destination="/".join([target, filename])
+    file.save(destination)
+    session['uploadFilePath']=destination
+    response="success"
+    return response
--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import Home from './Home';
 import Analytic from './Analytic';
 
 function App() {
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:5000/")
-      .then((response) => {
-        console.log("SUCCESS", response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="analytic" element={ <Analytic/> } />
-      </Routes>
-    </div>
-  );
+    useEffect(() => {
+        axios
+            .get("http://18.189.43.26:8080/")
+            .then((response) => {
+                console.log("SUCCESS", response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="analytic" element={<Analytic />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
