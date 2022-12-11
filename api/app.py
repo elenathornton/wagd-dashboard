542f339576c285c46c874dcf74464b3c4a57ad99
“lthornton3” <elena.thornton@gmail.com>
2022-12-11 13:08:36 -0500

    add upload

--- api/app.py
+++ api/app.py
@@ -1,9 +1,18 @@
+import numpy as np
+import pandas as pd
 from flask import Flask, request, session
 import os
+import csv
+from flask_cors import CORS, cross_origin
+
 
 api = Flask(__name__)
+cors = CORS(api)
+api.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @api.route('/')
+@cross_origin()
 def start():
     response_body = {
         "Status": "Server is live."
@@ -14,7 +23,7 @@ def start():
 
 @api.route('/upload', methods=['POST'])
 def fileUpload():
-    target=os.path.join('/','test_docs')
+    target=os.path.join('/','raw_data')
     if not os.path.isdir(target):
         os.mkdir(target)
     print("upload`")
@@ -26,3 +35,42 @@ def fileUpload():
     session['uploadFilePath']=destination
     response="success"
     return response
+
+
+def parse(file_name):
+    print("parse")
+    print(file_name
+          )
+    f=open("raw_data/"+file_name)
+    # skip = 3
+    new_row = []
+    for row in csv.reader(f):
+        for i, elem in enumerate(row):
+            elem = elem.replace(" ","")
+            if i!=0 and i%8==0 and i!=len(row)-1:
+                if elem.count('-') == 2:
+                    split_number = elem.split("-")
+                    new_row.append(-1*int(split_number[1]))
+                    new_row.append(-1*int(split_number[2]))
+                elif elem.count('-') == 1:
+                    if elem[0]=="-":
+                        # print(elem)
+                        new_row.append(int(elem[:4]))
+                        new_row.append(int(elem[4:]))
+                    else:
+                        split_number = elem.split("-")
+                        # print(elem)
+                        new_row.append(int(split_number[0]))
+                        new_row.append(-1 * int(split_number[1]))
+                elif elem.count('-') == 0:
+                    new_row.append(int(elem[:3]))
+                    new_row.append(int(elem[3:]))
+            else:
+                new_row.append(int(elem))
+
+    # print(len(new_row))
+    num_row = np.array(new_row)
+    num_row = num_row.reshape(int(len(new_row)/9),9)
+    # print(num_row)
+    df = pd.DataFrame(num_row, columns =['ax', 'ay', 'az', 'gx', 'gy', 'gz', 'yaw', 'pitch', 'roll'])
+    df.to_csv( "clean_data/" + file_name + ".csv", index = False)
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,62 @@
 import './Home.css';
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Home() {
-    return (
-        <div>
-            <header className="App-header">
-                <h1>Canine Athletics Dashboard</h1>
-                <p>Upload your .csv file to view analytics!</p>
-                <Link to="analytic"><button>Upload</button></Link>
-            </header>
-        </div>
-    );
+
+
+
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            imageURL: '',
+        };
+
+        this.handleUploadImage = this.handleUploadImage.bind(this);
+    }
+
+    handleUploadImage(ev) {
+        ev.preventDefault();
+
+        const data = new FormData();
+        data.append('file', this.uploadInput.files[0]);
+        data.append('filename', this.fileName.value);
+
+        fetch('http://18.189.43.26:8080/upload', {
+            method: 'POST',
+            body: data,
+        }).then((response) => {
+            response.json().then((body) => {
+                this.setState({ imageURL: `http://localhost:8000/${body.file}` });
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <header className="App-header">
+                    <h1>Canine Athletics Dashboard</h1>
+                    <p>Upload your .csv file to view analytics!</p>
+                    <form onSubmit={this.handleUploadImage}>
+                        <div>
+                            <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+                        </div>
+                        <div>
+                            <input ref={(ref) => { this.fileName = ref; }} type="text" placeholder="Enter the desired name of file" />
+                        </div>
+                        <br />
+                        <div>
+                            <Link to="analytic"><button>Upload</button></Link>
+                        </div>
+                    </form>
+                </header>
+            </div>
+        );
+    }
+
 }
-  
+
 export default Home;
