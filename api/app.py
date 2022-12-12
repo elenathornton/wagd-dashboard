7b1b83e6786b215796fd738ea5bea97716f8eb87
“lthornton3” <elena.thornton@gmail.com>
2022-12-11 19:09:22 -0500

    calc a

--- api/app.py
+++ api/app.py
@@ -35,10 +35,9 @@ def fileUpload():
     print(file)
     file_data = pd.read_csv(file.stream)
     # print(file_data)
-    print(file_data.columns)
+    print(file_data['ax'])
     filename = file.filename
     
-    file_data.apply(lambda f: print(f['ax']))
     
     file_data['raw_acceleration'] = file_data.apply(lambda x: math.sqrt(x['ax']**2 + x['ay']**2 + x['az']**2))
 
